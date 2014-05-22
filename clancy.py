fe8f6c49f006487d7695980fdd900c69509e0935
pliu <paul@ferociouspings.com>
2014-05-22 05:58:30 -0400

    temp

--- clancy.py
+++ clancy.py
@@ -2,16 +2,26 @@
 """clancy.py
 
 Usage:
-    clancy.py [-u <user>] create
-    clancy.py [-u <user>] delegate --time <time> --usages <usages>
-    clancy.py [-u <user>] modify --action <action> --target <target>
-    clancy.py [-u <user>] encrypt --owners <owners> --min <minimum> (--str <string> | --file <path>)
-    clancy.py [-u <user>] decrypt (--str <string> | --file <path>)
-    clancy.py [-u <user>] summary
-    clancy.py [-u <user>] password
+    clancy.py create 
+    clancy.py delegate --time <time> --usages <usages> 
+    clancy.py encrypt --owners <owners> --min <minimum> (--str <string> | --file <path>) 
+    clancy.py decrypt (--str <string> | --file <path>) 
+    clancy.py summary 
+    clancy.py password 
+    clancy.py modify --action <action> --target <target> 
 
     clancy.py --help
     clancy.py --version
+
+Commands:
+    create:    Create a new vault.
+    delegate:  Delegate your password to the server for a fixed time and number
+               of decryptions.
+    encrypt:   Allows an admin user to encrypt a piece of data.
+    decrypt:   Allows an admin to decrypt a piece of data.
+    summary:   Provides a list of users with keys on the system.
+    password:  Change your password.
+    modify:    Modify the state of another user.
 
 Options:
     --time <time>      Delegation time string.
@@ -25,14 +35,47 @@ Options:
     --str <string>     Base64 encoded string to operate on.
     --file <path>      Path to a file of base64 encoded data to operate on.
 
-    -u, --user <user>  User to log in as. Defaults to current user.
     -h, --help         Show this help message and exit.
     -v, --version      Show version and exit.
 """
 from docopt import docopt
+import os
+import sys
+import getpass
+import json
+import requests 
+
+
+def load_args():
+    config = {}
+    locations = [os.curdir, os.path.expanduser("~/.clancy"), "/etc/clancy"]
+    for loc in locations:
+        try:
+            with open(os.path.join(loc, "clancy.conf"), 'rb') as source:
+                config = json.load(source)
+                break
+        except IOError:
+            pass
+    return config
+
+
+def call_server(endpoint, payload):
+    pass
+
 
 VERSION = '0.0.1'
-
 if __name__ == '__main__':
-    args = docopt(__doc__, version=VERSION)
-    print(args)
+    cli_args = docopt(__doc__, version=VERSION)
+
+    # conf file args take precedence over cli args
+    args = dict(cli_args.items() + load_args().items())
+    if 'server' not in args:
+        sys.stderr.write('No server defined in config file. See Readme.')
+        sys.exit(1)
+    if 'user' not in args:
+        args['user'] = getpass.getuser()
+
+    if args['create']:
+
+
+
